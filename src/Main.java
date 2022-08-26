22eecb5a17d0878ae5d3b71bd88f2f485b02b6cd
joanna-maria <szacinska@gmail.com>
2022-08-26 21:19:04 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5,6));
+        System.out.println(calculator.subtract(10,4));
+    }
+}
